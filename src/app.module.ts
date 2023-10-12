18e85fe8e945fa9e39b438d2afe255f01e50537f
Diego Suntaxi <diego.suntaxi@outlook.es>
2023-10-11 23:30:37 -0500

    feat(tasks): crud operation draft

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
